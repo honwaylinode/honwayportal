a9dd5c6b7a9b16fa72c45f27f90b74e86c744913
jim zike huang <iotpump.developer@gmail.com>
2017-04-30 22:58:42 -0700

    update server script

--- start_client_server.sh
+++ start_client_server.sh
@@ -7,11 +7,11 @@ sh start_nginx.sh
 
 echo starting api server
 # now start the server and piple it to server.log
-~/.nvm/versions/node/v7.6.0/bin/node server/server.js dev > server.log 2>&1 &
+node --harmony-async-await server/server.js dev > server.log 2>&1 &
 
 echo starting web client hot loading
 # now start the web client 
 cd webclient
-npm run dev > ../webclient.log 2>&1 &
+http-server . > ../webclient.log 2>&1 &
 cd ..
 cd ..
